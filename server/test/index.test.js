182c1112deb155e9acda0f3b0d23613193dcadc4
tomjohnburton <tomjohnburton@gmail.com>
2018-12-30 20:03:11 +0100

    Unit test update

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,32 +1,32 @@
 // ! Basic test environment set up
 
-const User = require("../models/User");
-const Post = require("../models/Post");
-const expect = require("chai").expect; //? Testing framework
-const nock = require("nock"); //? HTTP server mocking and expectations library for Node.js
-const response = require("./response"); //? response folder
-const auth = require("../routes/auth");
-const uuidv4 = require("uuid/v4");
+const User = require('../models/User');
+const Post = require('../models/Post');
+const expect = require('chai').expect; //? Testing framework
+const nock = require('nock'); //? HTTP server mocking and expectations library for Node.js
+const response = require('./response'); //? response folder
+const auth = require('../routes/auth');
+const uuidv4 = require('uuid/v4');
 
-var app = require("../app");
-var request = require("supertest");
+var app = require('../app');
+var request = require('supertest');
 // require("../configs/database");
 
 //let's set up the data we need to pass to the login method
 const userCredentials = {
-  username: "tom@tom.tom",
-  password: "tom"
+  username: 'test',
+  password: 'test'
 };
 //now let's login the user
 var authenticatedUser = request.agent(app);
 before(function(done) {
   authenticatedUser
-    .post("/login")
+    .post('/login')
     .send(userCredentials)
     .end(function(err, response) {
       console.log(response.status);
       expect(response.status).to.equal(200);
-      expect("Location", "/");
+      expect('Location', '/');
       done();
     });
 });
@@ -34,26 +34,27 @@ before(function(done) {
 //after the POST has completed, make sure the status code is 200
 //also make sure that the user has been directed to the /home page
 
-describe("GET /", function(done) {
+describe('GET /', function(done) {
   //addresses 1st bullet point: if the user is logged in we should get a 200 status code
-  it("should return a 200 response if the user is logged in", function(done) {
-    authenticatedUser.get("/").expect(200, done);
+  it('should return a 200 response if the user is logged in', function(done) {
+    authenticatedUser.get('/').expect(200, done);
   });
 });
 
+let username = uuidv4();
 let signupCredentials = {
-  username: uuidv4() + "@example.com",
-  password: "example"
+  username: username,
+  password: 'example'
 };
 
 before(function(done) {
   authenticatedUser
-    .post("/signup")
+    .post('/signup')
     .send(signupCredentials)
     .end(function(err, response) {
       console.log(response.status);
       expect(response.status).to.equal(200);
-      expect("Location", "/");
+      expect('Location', '/');
       done();
     });
 });
@@ -61,9 +62,21 @@ before(function(done) {
 //after the POST has completed, make sure the status code is 200
 //also make sure that the user has been directed to the /home page
 
-describe("GET /", function(done) {
+describe('GET /', function(done) {
   //addresses 1st bullet point: if the user is logged in we should get a 200 status code
-  it("should return a 200 response if the user successfully signed up", function(done) {
-    authenticatedUser.get("/").expect(200, done);
+  it('should return a 200 response if the user successfully signed up', function(done) {
+    authenticatedUser.get('/').expect(200, done);
   });
+});
+
+// Delete the test user afterwards
+
+after(function(done) {
+  authenticatedUser
+    .post('/user-delete-test')
+    .send({ username })
+    .end(function(err, response) {
+      expect(response.status).to.equal(200, done);
+      done();
+    });
 });
